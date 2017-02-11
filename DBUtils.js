e45f2a0602f363ecb37cacf5c06903cf5e256484
LogosHealth <Healthlogos@gmail.com>
2017-02-10 16:07:49 -0800

    Add files via upload

---
+++ DBUtils.js
@@ -0,0 +1,49 @@
+/**
+ * LogosHealth App Database Utility. 
+ * This Util has all support functions for DB operations, uses SQL driver supported classes & utilities for persistence 
+ * Copyright Logos Health, Inc
+ * 
+ */
+
+//global variables
+var mysql = require('mysql');
+
+/**
+ * Create a new Connection instance.
+ * @param {object|string} config Configuration or connection string for new MySQL connection
+ * @return {Connection} A new MySQL connection
+ * @public
+ */
+exports.getDBConnection = function getDBConnection() {
+  console.log(' DBUtils.getDBConnection >>>>>>');
+    var connection = mysql.createConnection({
+        host     : 'logoshealth.cc99l18g9gw3.us-east-1.rds.amazonaws.com',
+        port      : '3306',
+        user     : 'logosadmin',
+        password : 'L0g0sH3alth'  //yet to encrypt password
+    });
+    
+    return connection;
+};
+
+/**
+ * Closes an existing connection.
+ * @param {object|string} an active connection object
+ * @return {boolean} Whether connection is closed or not
+ * @public
+ */
+exports.closeDBConnection = function closeDBConnection(connection) {
+  console.log(' DBUtils.closeDBConnection >>>>>>');
+  connection.end();
+};
+
+/**
+ * Debugs class instantiation.
+ * @param {none} 
+ * @return {boolean} Function could be called
+ * @public
+ */
+exports.checkClassAccess = function checkClassAccess() {
+  console.log(' DBUtils.checkClassAccess >>>>>>');
+  return true;
+};
---
+++ LogosHelper.js
@@ -0,0 +1,68 @@
+/**
+ * LogosHealth Application Utils. 
+ * All global re-usable/utility functions listed here. Class could be expanded to cover my utils
+ * JSON parsing is main 
+ * Copyright Logos Health, Inc
+ * 
+ */
+
+var aws = require('aws-sdk');
+
+/**
+ * Create a new build response.
+ * @param {object} [values]Session Attributes to use in add in response object
+ * @param {object} Speech output
+ * 
+ * @public
+ */
+exports.buildResponse = function buildResponse(sessionAttributes, speechletResponse) {
+	console.log(' LogosHelper.buildResponse >>>>>>');
+	return {
+        version: '1.0',
+        sessionAttributes,
+        response: speechletResponse,
+    };
+    
+};
+
+/**
+ * Provides a speech response to Alexa using JSON format.
+ * @param {object|string} Title of the Speech card
+ * @param {object|string} Speech output text 
+ * @param {object|string} To prompt speech out text
+ * @param {object|string} Whether session to be end or not
+ * @return {object} A JSON object constructed with speech out text
+ * @public
+ */
+exports.buildSpeechletResponse = function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+  	console.log(' LogosHelper.buildSpeechletResponse >>>>>>');
+  	return {
+        outputSpeech: {
+            type: 'PlainText',
+            text: output,
+        },
+        card: {
+            type: 'Simple',
+            title: `${title}`,
+            content: `${output}`,
+        },
+        reprompt: {
+            outputSpeech: {
+                type: 'PlainText',
+                text: repromptText,
+            },
+        },
+        shouldEndSession,
+    };
+};
+
+/**
+ * Debugs class instantiation.
+ * @param {none} 
+ * @return {boolean} Function could be called
+ * @public
+ */
+exports.checkClassAccess = function checkClassAccess() {
+  	console.log(' LogosHelper.checkClassAccess >>>>>>');
+  	return true;
+};
