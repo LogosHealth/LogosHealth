276f3f75784d9bb7128818463fb2d88bbc61cbe6
LogosHealth <Healthlogos@gmail.com>
2017-02-17 20:46:43 -0800

    Add files via upload

--- LogosHealthApp.js
+++ LogosHealthApp.js
@@ -11,15 +11,18 @@
 //global variables & Utils classes to import
 var dbUtil = require('./utils/DBUtils');
 var helper = require('./utils/LogosHelper');
+var request = require('request');
+
+var test = "";
 
 // --------------- Functions that control the skill's behavior -----------------------
 
 function getWelcomeResponse(callback) {
     // If we wanted to initialize the session to have some attributes we could add those here.
-    const sessionAttributes = {};
+    const sessionAttributes = {currentProcessor: 0};
     const cardTitle = 'LogosHealth App';
 
-    let speechOutput = 'Welcome to Logos Health personal health records.  Who am I speaking with today??", "Welcome';
+    let speechOutput = 'Logos Health personal health records.  Who am I speaking with today?", "Welcome';
 
     const repromptText = 'What can I help you with?';
     const shouldEndSession = false;
@@ -28,11 +31,53 @@ function getWelcomeResponse(callback) {
 
 }
 
-function verifyUserExists() {
-    //Check Database with profile name, if exists repond with greeting else propose a creation
-    console.log('Load All User Accounts from DB called >>>>>>');
-    var userAccounts = dbUtil.getAllUserAccounts();
-    console.log('All Accounts are >>>>>> ${userAccounts}');
+function identifyUser(callback) {
+    // If we wanted to initialize the session to have some attributes we could add those here.
+     console.log(' Identifying User >>>>>>');
+    const sessionAttributes = {currentProcessor: 1};
+    const cardTitle = 'LogosHealth App';
+    var userName = dbUtil.verifyUserProfile();
+    let speechOutput = "";
+    
+    if (userName === null || userName.length == 0) {
+    	speechOutput = 'Hello I could not found your profile. Would like to create one?';
+    } else {
+    	speechOutput = 'Hello '+userName+ ' How are you today? How can I help you?';
+    }
+
+    const repromptText = 'Menu Options';
+    const shouldEndSession = false;
+    
+    callback(sessionAttributes, helper.buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+
+}
+
+function processUserReponse(event, request, session, callback) {
+    console.log('Generic User response has been triggered >>>>>>');
+    var sessionAttributes = session.attributes;
+    
+    switch (sessionAttributes.currentProcessor) {
+    case 0:
+        identifyUser(callback);
+        break;
+    case 1:
+        //process method
+        break;
+    case 2:
+        //process method
+        break;
+    case 3:
+        //process method
+        break;
+    case 4:
+        //process method
+        break;
+    case 5:
+        //process method
+        break;
+    default: getWelcomeResponse(callback);
+    }
+    
 }
 
 function handleSessionEndRequest(callback) {
@@ -47,21 +92,16 @@ function handleSessionEndRequest(callback) {
 
 // --------------- Events -----------------------
 
-/**
- * Called when the session starts.
- */
-function onSessionStarted(sessionStartedRequest, session) {
-    console.log(`onSessionStarted requestId=${sessionStartedRequest.requestId}, sessionId=${session.sessionId}`);
-}
+
 
 /**
  * Called when the user launches the skill without specifying what they want.
  */
-function onLaunch(launchRequest, session, callback) {
+function onLaunch(event, launchRequest, session, callback) {
     //console.log(`onLaunch requestId=${launchRequest.requestId}, sessionId=${session.sessionId}`);
 
     //console.log('BeforeGetWelcomeResponse');
-    getWelcomeResponse(callback);
+    identifyUser(callback);
     //console.log('AfterGetWelcomeResponse: ' + callback);
 }
 
@@ -90,8 +130,12 @@ function processIntent(event, context, intentRequest, session, callback) {
     else if (intentName == 'AMAZON.CancelIntent')  {        
 	    quitRequest(intent, session, callback);  
     }
+    else if (intentName == 'LaunchIntent')  {        
+	    processUserReponse(event, intent, session, callback);  
+    }
     else {
-        throw "Invalid intent";
+        //throw "Invalid intent";
+        processUserReponse(event, intent, session, callback);
     }
 }
 
@@ -109,16 +153,22 @@ function handleOpenLogosHealthProfile (event, context, intent, session, callback
     const repromptText = 'Opening a profile';
     const shouldEndSession = false;
     
-    var accounts = verifyUserExists(event, context, callback);
+    var accounts = dbUtil.getAllUserAccounts();
     
     console.log(" The Accounts retrieved from Database >>>>"+accounts);
 
     callback(sessionAttributes, helper.buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
 }
 
-function createOrUpdateAppLink (event, context, request, appID) {
-    //get APP ID from helper
-    var appId = helper.createOrUpdateAppLink(event, context, request, appID);
+function checkUserMainProfileExists (event, session, callback) {
+    const sessionAttributes = {};
+    const cardTitle = 'Open Profile';
+
+    let speechOutput = 'Hello Marty, How are you today?", "Welcome to Logos Health App';
+
+    const repromptText = 'Opening a profile';
+    const shouldEndSession = false;
+    var isExists = helper.verifyUserProfile("","");
 }
 
 function handleCreateLogosHealthProfile (event, context, intent, session, callback) {
@@ -133,7 +183,7 @@ function handleCreateLogosHealthProfile (event, context, intent, session, callba
     const shouldEndSession = false;
     
     //TODO: Implement Create Profile DB logic here
-    
+    //dbUtil.createHealthProfile(passparams);
     
     callback(sessionAttributes, helper.buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
 }
@@ -146,15 +196,47 @@ function quitRequest (intent, session, callback) {
     //TODO: Implementation
 }
 
+function getAccountLinkName (event) {
+	var appName = "";
+	var amznProfileURL = 'https://api.amazon.com/user/profile?access_token=';
+    amznProfileURL += event.session.user.accessToken;
+    request(amznProfileURL, function(error, response, body) {
+        if (response.statusCode == 200) {
+             var profile = JSON.parse(body);
+             console.log('printing profile' + profile);
+             appName = profile.email;
+             console.log("LogosHelper >>>> Profile is  " + profile.toString());
+             console.log("LogosHelper >>>> :Hello, " + profile.email);
+                
+         } else {
+                console.log("LogosHelper >>>> : I can't connect to Amazon Profile Service right now, try again later");
+                appName = 'NO_ID';
+                
+         }
+     });
+     
+     console.log(" The Test from Account Link >>>> "+test);
+     
+     return appName;
+}
+
 /**
  * Called when the user ends the session.
  * Is not called when the skill returns shouldEndSession=true.
  */
 function onSessionEnded(sessionEndedRequest, session) {
-    console.log(`onSessionEnded requestId=${sessionEndedRequest.requestId}, sessionId=${session.sessionId}`);
-    // Add cleanup logic here
+    console.log('onSessionEnded requestId=${sessionEndedRequest.requestId}, sessionId=${session.sessionId}');
+    
 }
 
+/**
+ * Called when the session starts.
+ */
+function onSessionStarted(event, sessionStartedRequest, session) {
+    console.log('onSessionStarted requestId=${sessionStartedRequest.requestId}, sessionId=${session.sessionId}');
+    var appName = getAccountLinkName(event);
+    console.log(" The App name found is >>>>>> "+test);
+}
 
 // --------------- Main handler -----------------------
 
@@ -170,12 +252,12 @@ exports.handler = (event, context, callback) => {
         
         if (event.session.new) {           
             console.log('New Session created >>>>>');
-            onSessionStarted({ requestId: event.request.requestId }, event.session);
+            onSessionStarted(event, { requestId: event.request.requestId }, event.session);
         }
 
         if (event.request.type === 'LaunchRequest') {
             console.log('Launch Request processing >>>>>.');
-            onLaunch(event.request,event.session,
+            onLaunch(event,event.request,event.session,
                 (sessionAttributes, speechletResponse) => {
                     callback(null, helper.buildResponse(sessionAttributes, speechletResponse));
                 });
